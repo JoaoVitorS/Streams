f0ccd41320e18010992e9f23eb63ba7e2b297f09
Joao Vitor <=>
2021-08-11 20:10:54 -0300

    add functions CRUD

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,4 +1,5 @@
 import streams from '../api/streams';
+import history from '../history';
 import {
     SIGN_IN,
     SIGN_OUT,
@@ -29,6 +30,7 @@ export const createStream = formValues => async (dispatch, getState) => {
     const response = await streams.post('/streams', {...formValues, userId});
 
     dispatch({ type: CREATE_STREAM, payload: response.data });
+    history.push('/');
   } catch(err) { 
     throw err;
   };
@@ -57,9 +59,10 @@ export const fetchStream = id => async dispatch => {
 
 export const editStream = (id,formValues) => async dispatch => { 
   try {
-    const response = await streams.put(`/streams/${id}`, formValues);
+    const response = await streams.patch(`/streams/${id}`, formValues);
 
     dispatch({ type: EDIT_STREAM, payload: response.data });
+    history.push('/');
   } catch(err) { 
     throw err;
   }
@@ -70,6 +73,7 @@ export const deleteStream = id => async dispatch => {
     await streams.delete(`/streams/${id}`);
 
     dispatch({ type: DELETE_STREAM, payload: id });
+    history.push('/');
   } catch(err) { 
     throw err;
   };
